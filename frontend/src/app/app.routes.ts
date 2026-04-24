cd5290dd3e4c619e0e821e2d7590cc533d469dda
Fanoa <fanoalessio1988@hotmail.com>
2026-04-24 22:26:36 +0200

    feat(frontend): afficher les matchs publics

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,8 +2,8 @@ import { Routes } from '@angular/router';
 import { authGuard } from './core/auth/auth.guard';
 import { LoginPageComponent } from './features/auth/login/login-page.component';
 import { HomePageComponent } from './features/home/home-page.component';
+import { PublicMatchesPageComponent } from './features/matches/public-matches/public-matches-page.component';
 import { AdminPageComponent } from './features/placeholders/admin-page.component';
-import { MatchsPageComponent } from './features/placeholders/matchs-page.component';
 import { EspacePrivePageComponent } from './features/test/espace-prive-page.component';
 import { MainLayoutComponent } from './layout/main-layout.component';
 
@@ -15,7 +15,7 @@ export const routes: Routes = [
       { path: '', component: HomePageComponent },
       { path: 'login', component: LoginPageComponent },
       { path: 'espace-prive', component: EspacePrivePageComponent, canActivate: [authGuard] },
-      { path: 'matchs', component: MatchsPageComponent, canActivate: [authGuard] },
+      { path: 'matchs', component: PublicMatchesPageComponent, canActivate: [authGuard] },
       { path: 'admin', component: AdminPageComponent, canActivate: [authGuard] }
     ]
   },
